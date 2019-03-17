c390ccaf7412c9f4c37b1ef0a5206b35d1c06e07
Jeffery Cosby <jcoz1968@gmail.com>
2019-03-16 23:05:24 -0500

    sass, global styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,10 @@
 import React from "react"
+import styles from "./404.module.scss";
 
 export default () => (
-  <div>
-    <h1>Page not found</h1>
-    <p>
+  <div className="content">
+    <h1 className={styles.header}>Page not found</h1>
+    <p className={styles.errorMessage}>
       The page you are looking for does not exist.
     </p>
   </div>
