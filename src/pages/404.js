ffa3041e7e1b16297efa7d89659b5dd8ad0e4204
Jeffery Cosby <jcoz1968@gmail.com>
2019-03-16 22:36:34 -0500

    about, 404 pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,9 @@ import React from "react"
 
 export default () => (
   <div>
-    <h1>This is the index page</h1>
+    <h1>Page not found</h1>
     <p>
-      Lorem ipsum dolor sit amet.
+      The page you are looking for does not exist.
     </p>
   </div>
 )
